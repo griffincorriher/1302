e7278f0bb68d763a0891b1e06f9639a97c2eaca8
griffincorriher <57311125+griffincorriher@users.noreply.github.com>
2021-11-19 13:17:04 -0500

    Update playgame.py

--- playgame.py
+++ playgame.py
@@ -1,13 +1,11 @@
 from Card import Card, Hand, Deck
 
-play = True
 game_start = False
-max_points = 21
+MAX_POINTS = 21
 player = ''
-hands = {}
 
 def restart():
-    play = True
+    global play
     print("Do you want to play blackjack?\n")
     x = input("Enter Y for Yes and N for No: \n")
     if x.upper() == "Y":
@@ -20,85 +18,67 @@ def restart():
       restart()
     return play  
 
-def initialize_game():
-    game_start = True
-    player = input("What is your name?\n")
-    #Input how many computer opponents not including dealer
-    game_deck = Deck()
-    game_deck.shuffle()
-    
-    players = [player, 'Dealer', 'Computer1', 'Computer2']
-    all_hands = []
-    for i in range(len(players)):
-        players[i] = Hand()
-        players[i].add(game_deck.deal())
-        players[i].add(game_deck.deal())
-        all_hands.append(players[i])
-        
-    player_hand = all_hands[0]
-    dealer_hand = all_hands[1]
-    computer1_hand = all_hands[2]
-    computer2_hand = all_hands[3]
-    print(player_hand)
-    player_hand.PrintWithIndexes()
-    print(player_hand)
-
-    #create player hand
-#    player_hand = Hand()
-#    player_hand.add(game_deck.deal())
-#    player_hand.add(game_deck.deal())
-
-    #create dealer hand
-#    dealer_hand = Hand()
-#    dealer_hand.add(game_deck.deal())
-#    dealer_hand.add(game_deck.deal())
-
-    #create computer hands for every n computer players
-    #they didn't specify how many players, we could put
-    #into for loop to make 'n' number of players but I think we just make 2
-#    computer1_hand = Hand()
-#    computer1_hand.add(game_deck.deal())
-#    computer1_hand.add(game_deck.deal())
-    
-#    computer2_hand = Hand()
-#    computer2_hand.add(game_deck.deal())
-#    computer2_hand.add(game_deck.deal())
-
-#    print(player_hand)
-    print("All hands have been dealt")
-    return game_start
-
 def play_game():
     print()
 
+#def convert_hand(player):
+#    if(player.get_ranks() >=11):
+        
+
 def hit(player):
-    #add another card to hand
-    print('ok')
+    #add another card to hand, sum cards in hand and return value
+    player.add(game_deck.deal())
+    points = sum(player.get_ranks())
+    return points
     
 def stand(player):
-    #return hand numerical value
-    print('ok')
+    #return hand numerical value after standing
+    points = sum(player.get_ranks())
+    return points
     
 def compare(player):
-    #compare player hands
+    #compare player hands against max_points
     print('ok')
 
-def test():
-    game_deck = Deck()
-    game_deck.shuffle()    
-    players = Hand()
-    players.add(game_deck.deal())
-    players.add(game_deck.deal())    
-    players.PrintWithIndexes()
+def display_dealer_hand():
+    dealer_hand.add(dealers_first_card)
+    points = sum(dealer_hand.get_ranks())
+    return points
+
+def display_hands():
+    for name, i in zip(player_names, hands_list):
+        print(name,"'s hand:\n",i, "\n")
     
-play = restart()    
-while(play == True):
-    game_start = initialize_game()
-    print(game_start)
-    while(game_start == True):
-#        play_game()
-        test()
-       
+## Running Code ##    
+player = input("What is your name?\n")
+#Input how many computer opponents not including dealer
+game_deck = Deck()
+game_deck.shuffle()
+player_names = ['Dealer', player, 'Computer1', 'Computer2']
+hands = [None] * len(player_names)
+all_hands = []
+
+for i in range(len(hands)):
+    hands[i] = Hand()
+    hands[i].add(game_deck.deal())
+    hands[i].add(game_deck.deal())
+    all_hands.append(hands[i])
     
-    
-        #play = restart()
+dealer_hand = all_hands[0]
+dealers_first_card = dealer_hand.discard_top()    
+player_hand = all_hands[1]
+computer1_hand = all_hands[2]
+computer2_hand = all_hands[3]
+
+hands_list = [dealer_hand, player_hand, computer1_hand, computer2_hand]
+
+display_hands()
+
+
+print("All hands have been dealt")
+
+
+hit(player_hand)
+display_hands()
+
+#play = restart()
